e9da9a0dba3fafd36361fa8f6e17b0466ecddfb2
mmrahimi <rahimi1.mohammadmahdi@gmail.com>
2025-12-21 19:56:45 +0330

    refactor: change the returned fields in the subject resource class

--- app/Http/Resources/SubjectResource.php
+++ app/Http/Resources/SubjectResource.php
@@ -15,8 +15,8 @@ class SubjectResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
-            'user' => new UserResource($this->user),
         ];
     }
 }
